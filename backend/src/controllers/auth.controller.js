faad2815da9602e97af71f0c2b17d83b366ea541
凌岳群 <raven@lingyuequndeMacBook-Air.local>
2025-10-29 01:52:09 +0800

    login & logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,3 +63,36 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' })
   }
 }
+
+export const login = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' })
+
+    // never tell the client which one is incorrect: password or email
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: 'Invalid credentials' })
+
+    generateToken(user.id, res)
+
+    res.status(200).json({
+      id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    })
+  } catch (error) {
+    console.error('Error in login controller: ', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+export const logout = async (_, res) => {
+  res.cookie('jwt', '', { maxAge: 0 })
+  res.status(200).json({ message: 'Logged out successfully' })
+}
