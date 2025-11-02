1fa1ceefc09f8fc39fe3710778307602cbb35748
凌岳群 <raven@lingyuequndeMacBook-Air.local>
2025-11-03 01:17:30 +0800

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create(set => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
@@ -32,6 +33,32 @@ export const useAuthStore = create(set => ({
       )
     } finally {
       set({ isSigningUp: false })
+    }
+  },
+  login: async data => {
+    set({ isLoggingIn: true })
+
+    try {
+      const res = await axiosInstance.post('/auth/login', data)
+
+      set({ authUser: res.data })
+      toast.success('Logged in successfully!')
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || 'An error occurred during login'
+      )
+    } finally {
+      set({ isLoggingIn: false })
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post('/auth/logout')
+      set({ authUser: null })
+      toast.success('Logged out successfully!')
+    } catch (error) {
+      toast.error('Error logging out')
+      console.error('Logout error: ', error)
     }
   }
 }))
