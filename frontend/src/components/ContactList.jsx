b2acb38edfd4404bb88d9990a4fa53805d61f5be
凌岳群 <raven@lingyuequndeMacBook-Air.local>
2025-11-06 00:04:49 +0800

    sidebar completed

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import UserLoadingSkeleton from './UsersLoadingSkeleton'
+import NoChatsFound from './NoChatsFound'
 
 const ContactList = () => {
+  const { getAllContacts, allContacts, isUsersLoading, setSelectedUser } =
+    useChatStore()
+
+  useEffect(() => {
+    getAllContacts()
+  }, [getAllContacts])
+
+  if (isUsersLoading) return <UserLoadingSkeleton />
+  if (allContacts.length === 0) return <NoChatsFound />
+
   return (
-    <div>ContactList</div>
+    <>
+      {allContacts.map(contact => (
+        <div
+          key={contact.Id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            {/* TODO: MAKE IT WORK WITH SOCKET */}
+            <div className={`avatar avatar-online`}>
+              <div className="size-12 rounded-full">
+                <img
+                  src={contact.profilePic || '/avatar.png'}
+                  alt={contact.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {contact.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
